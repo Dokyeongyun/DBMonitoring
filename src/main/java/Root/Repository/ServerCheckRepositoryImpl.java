925536809522970d60f9d18587b21fbee56f31e2
DoKyeongYun <aservmz@naver.com>
2021-10-07 03:42:00 +0900

    특정 기간동안의 Alert Log만 모니터링하는 기능 개발(타임스탬프가 yyyy-MM-ddT Format으로 기록되는 경우만 가능)

--- src/main/java/Root/Repository/ServerCheckRepositoryImpl.java
+++ src/main/java/Root/Repository/ServerCheckRepositoryImpl.java
@@ -1,6 +1,10 @@
 package Root.Repository;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 
@@ -10,6 +14,7 @@ import com.jcraft.jsch.Session;
 
 import Root.Model.AlertLogCommand;
 import Root.RemoteServer.JschUtil;
+import Root.Utils.DateUtils;
 
 public class ServerCheckRepositoryImpl implements ServerCheckRepository {
 	private JschUtil jsch;
@@ -54,6 +59,66 @@ public class ServerCheckRepositoryImpl implements ServerCheckRepository {
 			Channel channel = jsch.openExecChannel(session, alc.getCommand());
 			InputStream in = jsch.connectChannel(channel);
 			result = IOUtils.toString(in, "UTF-8");
+			jsch.disConnectChannel(channel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return result;
+	}
+	
+	@Override
+	public String checkAlertLogDuringPeriod(AlertLogCommand alc) {
+		System.out.println("ALC READ LINE: "+alc.getReadLine());
+		String result = "";
+		try {
+			Session session = this.getSession();
+			session = this.connectSession(session);
+			Channel channel = jsch.openExecChannel(session, alc.getCommand());
+			InputStream in = jsch.connectChannel(channel);
+			result = IOUtils.toString(in, "UTF-8");
+			
+			String startDate = DateUtils.addDate(new Date(), "yyyy-MM-dd", 0, 0, -1);
+			String endDate = DateUtils.getToday("yyyy-MM-dd");
+			
+			String dateRegEx = "\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T"; 
+			
+			while(true) {
+				Matcher dates = Pattern.compile(dateRegEx).matcher(result);	// 'yyyy-MM-ddT' 포맷의 날짜를 찾아냄
+				if(dates.find()) {
+					String logDate = dates.group();
+					long dateDiff = DateUtils.getDateDiffTime("yyyy-MM-dd", logDate, startDate);
+					if(dateDiff >= 0) {
+						// 조회 Line 수를 더 늘려서 다시 조회
+						alc.setReadLine(String.valueOf(Integer.parseInt(alc.getReadLine()) * 2));
+						result = this.checkAlertLog(alc);
+					} else {
+						break;
+					}
+				}
+			}
+			
+			boolean isStartDay = false;
+			StringBuffer sb = new StringBuffer();
+			
+			dateRegEx = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T"; 
+			
+			String[] lines = result.split("\n");
+			for(String line : lines) {
+				String linePrefix = line.substring(0, line.length() < 11 ? line.length() : 11);
+				if(Pattern.matches(dateRegEx, linePrefix)) {
+					if(linePrefix.startsWith(startDate)) {
+						isStartDay = true;
+					}
+				}
+				
+				if(isStartDay) {
+					sb.append(line);
+				}
+			}
+			
+			result = sb.toString();
+			
 			jsch.disConnectChannel(channel);
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/main/java/Root/Utils/DateUtils.java
+++ src/main/java/Root/Utils/DateUtils.java
@@ -1,5 +1,6 @@
 package Root.Utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -10,9 +11,10 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class DateUtils {
-	
+
 	/**
 	 * 오늘 날짜 및 시간을 지정한 format에 따라 반환한다.
+	 * 
 	 * @param format
 	 * @return
 	 */
@@ -23,26 +25,29 @@ public class DateUtils {
 
 	/**
 	 * 매개변수로 주어진 년도, 달의 총 일수를 반환한다.
+	 * 
 	 * @param year
 	 * @param month
 	 * @return
 	 */
 	public static int getMonthlyDayCount(int year, int month) {
-	    Calendar cal = new GregorianCalendar(year, --month, 1);
-	    return cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		Calendar cal = new GregorianCalendar(year, --month, 1);
+		return cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 	}
-	
+
 	/**
 	 * 월, 일의 값을 두 자리로 변환하여 반환한다.
+	 * 
 	 * @param value
 	 * @return
 	 */
 	public static String getTwoDigitDate(int value) {
-		return value < 10 ? "0"+value : String.valueOf(value);
+		return value < 10 ? "0" + value : String.valueOf(value);
 	}
-	
+
 	/**
 	 * 주어진 일자의 요일을 반환한다.
+	 * 
 	 * @param year
 	 * @param month
 	 * @param day
@@ -55,9 +60,10 @@ public class DateUtils {
 		DayOfWeek dayOfWeek = date.getDayOfWeek();
 		return dayOfWeek.getDisplayName(style, locale);
 	}
-	
+
 	/**
 	 * 주어진 일자가 주말인지 확인한다.
+	 * 
 	 * @param year
 	 * @param month
 	 * @param day
@@ -66,6 +72,47 @@ public class DateUtils {
 	public static boolean isWeekEnd(int year, int month, int day) {
 		LocalDate date = LocalDate.of(year, month, day);
 		DayOfWeek dayOfWeek = date.getDayOfWeek();
-		return dayOfWeek.getValue() == 6 ? true : dayOfWeek.getValue() == 7 ? true : false;  
+		return dayOfWeek.getValue() == 6 ? true : dayOfWeek.getValue() == 7 ? true : false;
+	}
+
+	/**
+	 * 기준일자에서 년, 월, 일을 더한 날짜를 반환한다.
+	 * 빼기도 가능하다.
+	 * @param curDate		기준일자
+	 * @param returnFormat 	반환받을 날짜포맷
+	 * @param year			
+	 * @param month
+	 * @param day
+	 * @return
+	 */
+	public static String addDate(Date curDate, String returnFormat, int year, int month, int day) {
+		SimpleDateFormat sdf = new SimpleDateFormat(returnFormat);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(curDate);
+		cal.add(Calendar.YEAR, year);
+		cal.add(Calendar.MONTH, month);
+		cal.add(Calendar.DATE, day);
+		return sdf.format(cal.getTime());
+	}
+	
+	/**
+	 * 두 날짜간 차이를 반환한다. (첫번째날짜 - 두번째날짜)
+	 * 이 때, 각 날짜의 포맷은 첫번째 인자로 전달한 dateFormat과 동일해야한다.
+	 * @param dateFormat
+	 * @param date1
+	 * @param date2
+	 * @return
+	 */
+	public static long getDateDiffTime(String dateFormat, String date1, String date2) {
+		long diffTime = 0;
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+		try {
+	        Date FirstDate = sdf.parse(date1);
+	        Date SecondDate = sdf.parse(date2);
+	        diffTime = FirstDate.getTime() - SecondDate.getTime(); 
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return diffTime / 1000;
 	}
 }
