85b5180608aa46f997341278e227669f81c938fc
KyeongYun Do <aservmz@naver.com>
2022-01-09 23:50:39 +0900

    Mapping fields with csv columns in ArchiveUsage model

--- src/main/java/Root/core/domain/ArchiveUsage.java
+++ src/main/java/Root/core/domain/ArchiveUsage.java
@@ -2,6 +2,8 @@ package root.core.domain;
 
 import java.util.List;
 
+import com.opencsv.bean.CsvBindByName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +13,25 @@ import lombok.NoArgsConstructor;
 @Data
 public class ArchiveUsage {
 
+    @CsvBindByName(column = "NAME")
 	private String archiveName;
+
+    @CsvBindByName(column = "NumberOfFiles")
 	private int numberOfFiles;
+    
+    @CsvBindByName(column = "LimitSpace(G)")
 	private UnitString totalSpace;
+    
+    @CsvBindByName(column = "ReclaimableSpace(G)")
 	private UnitString reclaimableSpace;
+    
+	@CsvBindByName(column = "UsedSpace(G)")
 	private UnitString usedSpace;
+	
+	@CsvBindByName(column = "Used(%)")
 	private UnitString usedPercent;
+	
+	@CsvBindByName(column = "DNT")
 	private String dnt;
 	
 	public static String toCsvString(List<ArchiveUsage> list) {
