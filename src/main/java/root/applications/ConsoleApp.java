879dc9a536209ba1f255fa49152e6e325eff4af0
KyeongYun Do <aservmz@naver.com>
2022-02-18 00:54:50 +0900

    Load configuration file

--- src/main/java/root/applications/ConsoleApp.java
+++ src/main/java/root/applications/ConsoleApp.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import root.utils.PatternUtils;
+import root.utils.PropertiesUtils;
 
 /**
  * ConsoleApp은 Console을 통해 입/출력을 수행합니다.
@@ -16,12 +18,14 @@ import java.util.stream.Collectors;
  *
  */
 public class ConsoleApp {
-	
-	public static final String DEFAULT_CONFIG_DIR = "./config";
-	public static final Pattern IS_ONLY_NUMBER = Pattern.compile("^[0-9]*?");
 
-	public static void main(String[] args) {
+	public static final String DEFAULT_CONFIG_DIR = "./config/connectioninfo";
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// STEP1: 접속정보 설정파일 선택
+		String selectedFile = "";
 		while (true) {
 			System.out.println("접속정보 설정파일을 선택해주세요.");
 			List<String> configFiles = Arrays.asList(new File(DEFAULT_CONFIG_DIR).list())
@@ -38,45 +42,32 @@ public class ConsoleApp {
 				System.out.println(String.format("[%d] %s", (i + 1), configFiles.get(i)));
 			}
 
-			try {
-				String input = br.readLine().trim();
-				if(!isOnlyNumber(input)) {
-					System.out.println("잘못 입력하셨습니다. 접속정보 설정파일을 다시 선택해주세요.");
-					continue;
-				}
-				
-				int selectedId = Integer.valueOf(input);
-				if (!isOnlyNumber(input) || selectedId <= 0 || selectedId > configFiles.size()) {
-					System.out.println("잘못 입력하셨습니다. 접속정보 설정파일을 다시 선택해주세요.");
-				} else {
-					// TODO Show menu
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+			String input = br.readLine().trim();
+			if(!PatternUtils.isOnlyNumber(input)) {
+				System.out.println("잘못 입력하셨습니다. 접속정보 설정파일을 다시 선택해주세요.");
+				continue;
 			}
-			
-		}
 
-//			String propertiesFilePath = ".\\config\\application.properties";
-//
-//			try {
-//				PropertiesUtils.loadCombinedConfiguration();
-//				String lastUsePropertiesFile = PropertiesUtils.combinedConfig.getString("filepath.config.lastuse");
-//				PropertiesUtils.loadAppConfiguration(lastUsePropertiesFile, "connInfoConfig");
-//				PropertiesUtils.loadAppConfiguration(propertiesFilePath);
-//				PropertiesUtils.loadCombinedConfiguration();
-//				PropertiesUtils.loadAppConfiguration(".\\config\\connectioninfo\\test.properties", "connInfoConfig");
-//			} catch (Exception e) {
-//				System.out.println("configuration loading error\n" + e + "\n");
-//				return;
-//			}
-//
-//			String dbMonitoring = PropertiesUtils.propConfig.getString("monitoring.db");
-//			String serverMonitoring = PropertiesUtils.propConfig.getString("monitoring.server");
+			int selectedId = Integer.valueOf(input);
+			if (!PatternUtils.isOnlyNumber(input) || selectedId <= 0 || selectedId > configFiles.size()) {
+				System.out.println("잘못 입력하셨습니다. 접속정보 설정파일을 다시 선택해주세요.");
+				continue;
+			}
+
+			selectedFile = configFiles.get(selectedId - 1);
+			break;
+		}
+		System.out.println(String.format("선택된 파일은 [%s] 입니다.", selectedFile));
+
+		// STEP2: 선택된 접속정보 설정파일 Load
+		String propertiesFilePath = DEFAULT_CONFIG_DIR + "/" + selectedFile;
+		try {
+			PropertiesUtils.loadAppConfiguration(propertiesFilePath, "connInfoConfig");
+		} catch (Exception e) {
+			System.out.println("configuration loading error\n" + e + "\n");
+			return;
+		}
+		
+		// STEP3: 접속정보 설정파일 내, 모니터링여부 설정파일 찾기 및 LOAD
 	}
-	
-    public static boolean isOnlyNumber(final String str) {
-    	if(str == "") return false;
-        return IS_ONLY_NUMBER.matcher(str).matches();
-    }
 }
