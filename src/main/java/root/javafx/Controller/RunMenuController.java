e41310ab85afbda7ad5f844c4c4809e74271960f
KyeongYun Do <aservmz@naver.com>
2022-02-26 17:53:14 +0900

    UI/UX: Create 'CustomTreeView' for previewing connection info list in run menu

--- src/main/java/root/javafx/Controller/RunMenuController.java
+++ src/main/java/root/javafx/Controller/RunMenuController.java
@@ -1,16 +1,32 @@
 package root.javafx.Controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import root.javafx.CustomView.CustomTreeView;
 
 public class RunMenuController implements Initializable {
 
+	@FXML
+	AnchorPane connInfoSettingAP;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
 
+		CustomTreeView ctv = new CustomTreeView();
+		ctv.addTreeItem("DB 立加沥焊", new ArrayList<>(Arrays.asList("DB1", "DB2", "DB3")), FontAwesomeIcon.DATABASE);
+		ctv.addTreeItem("Server 立加沥焊", new ArrayList<>(Arrays.asList("Server1", "Server2", "Server3", "Server4")),
+				FontAwesomeIcon.SERVER);
+		AnchorPane.setTopAnchor(ctv, 40.0);
+		AnchorPane.setBottomAnchor(ctv, 0.0);
+		AnchorPane.setLeftAnchor(ctv, 0.0);
+		AnchorPane.setRightAnchor(ctv, 0.0);
+		connInfoSettingAP.getChildren().add(ctv);
+	}
 }
---
+++ src/main/java/root/javafx/CustomView/CustomTreeView.java
@@ -0,0 +1,51 @@
+package root.javafx.CustomView;
+
+import java.util.List;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.paint.Paint;
+
+public class CustomTreeView extends TreeView<String> {
+
+	private static TreeItem<String> rootItem;
+
+	private static final String DEFAULT_ICON_COLOR = "#003b8e";
+	private static final int DEFAULT_ICON_SIZE = 17;
+
+	private static final String LEAF_ICON_COLOR = "black";
+	private static final int LEAF_ICON_SIZE = 3;
+
+	public CustomTreeView() {
+		getStylesheets().add(System.getProperty("resourceBaseDir") + "/css/treeView.css");
+		getStyleClass().add("treeView");
+
+		rootItem = new TreeItem<>("접속정보 리스트", getIconView(FontAwesomeIcon.LIST));
+		rootItem.setExpanded(true);
+		setRoot(rootItem);
+
+		// this.setCellFactory(treeView -> new MyTreeCell());
+	}
+
+	public void addTreeItem(String title, List<String> items, FontAwesomeIcon icon) {
+		TreeItem<String> newTreeItem = new TreeItem<>(title, getIconView(icon));
+		for (String item : items) {
+			newTreeItem.getChildren()
+					.add(new TreeItem<>(item, getIconView(FontAwesomeIcon.CIRCLE, LEAF_ICON_SIZE, LEAF_ICON_COLOR)));
+		}
+		newTreeItem.setExpanded(true);
+		rootItem.getChildren().add(newTreeItem);
+	}
+
+	private FontAwesomeIconView getIconView(FontAwesomeIcon icon, int size, String color) {
+		FontAwesomeIconView result = new FontAwesomeIconView(icon, String.valueOf(size));
+		result.setFill(Paint.valueOf(color));
+		return result;
+	}
+
+	private FontAwesomeIconView getIconView(FontAwesomeIcon icon) {
+		return getIconView(icon, DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR);
+	}
+}
