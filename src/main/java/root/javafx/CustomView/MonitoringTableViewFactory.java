639e0e12553ca6989f35be1714008ab2629a6ac2
KyeongYun Do <aservmz@naver.com>
2022-03-12 21:57:33 +0900

    Add monitoring result table view factory for separating creation of tableview

---
+++ src/main/java/root/javafx/CustomView/MonitoringTableViewFactory.java
@@ -0,0 +1,37 @@
+package root.javafx.CustomView;
+
+import root.core.domain.ASMDiskUsage;
+import root.core.domain.ArchiveUsage;
+import root.core.domain.MonitoringResult;
+import root.core.domain.OSDiskUsage;
+import root.core.domain.TableSpaceUsage;
+import root.core.domain.enums.UsageUIType;
+
+public class MonitoringTableViewFactory {
+
+	private MonitoringTableViewFactory() {
+	}
+
+	public static <T extends MonitoringResult> MonitoringTableView<T> create(Class<T> clazz, UsageUIType usageUIType) {
+		MonitoringTableView<T> tableView = new MonitoringTableView<>();
+
+		if (clazz == ArchiveUsage.class) {
+			tableView.addColumn("Archive", "archiveName");
+			tableView.addColumn("사용량(%)", "usedPercent");
+		} else if (clazz == TableSpaceUsage.class) {
+			tableView.addColumn("테이블스페이스", "tableSpaceName");
+			tableView.addColumn("사용량(%)", "usedPercent");
+		} else if (clazz == ASMDiskUsage.class) {
+			tableView.addColumn("디스크 그룹", "asmDiskGroupName");
+			tableView.addColumn("디스크 타입", "asmDiskGroupType");
+			tableView.addColumn("사용량(%)", "usedPercent");
+		} else if (clazz == OSDiskUsage.class) {
+			tableView.addColumn("파일 시스템", "fileSystem");
+			tableView.addColumn("마운트 위치", "mountedOn");
+			tableView.addColumn("사용량(%)", "usedPercent");
+		}
+
+		tableView.setUsageUIType(usageUIType);
+		return tableView;
+	}
+}
