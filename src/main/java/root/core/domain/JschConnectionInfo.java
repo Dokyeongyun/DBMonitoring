852871425799202cb53b13073256ef6c5d7adbc0
KyeongYun Do <aservmz@naver.com>
2022-04-09 17:49:40 +0900

    Refactoring: Change field data type of 'port' in 'JschConnectionInfo' Model from String to int

--- src/main/java/root/core/domain/JschConnectionInfo.java
+++ src/main/java/root/core/domain/JschConnectionInfo.java
@@ -6,16 +6,16 @@ import lombok.Data;
 public class JschConnectionInfo {
 	private String serverName;
 	private String host;
-	private String port;
+	private int port;
 	private String userName;
 	private String password;
 	private AlertLogCommand alc;
-	
+
 	public JschConnectionInfo() {
 		this.alc = new AlertLogCommand();
 	}
 
-	public JschConnectionInfo(String serverName, String host, String port, String userName, String password) {
+	public JschConnectionInfo(String serverName, String host, int port, String userName, String password) {
 		this.serverName = serverName;
 		this.host = host;
 		this.port = port;
@@ -24,13 +24,32 @@ public class JschConnectionInfo {
 		this.alc = new AlertLogCommand();
 	}
 
+	public JschConnectionInfo(String serverName, String host, String port, String userName, String password) {
+		this(serverName, host, 22, userName, password);
+		this.setPort(port);
+	}
+
+	public JschConnectionInfo(String serverName, String host, int port, String userName, String password,
+			AlertLogCommand alc) {
+		this(serverName, host, port, userName, password);
+		this.alc = alc;
+	}
+
 	public JschConnectionInfo(String serverName, String host, String port, String userName, String password,
 			AlertLogCommand alc) {
-		this.serverName = serverName;
-		this.host = host;
-		this.port = port;
-		this.userName = userName;
-		this.password = password;
-		this.alc = alc;
+		this(serverName, host, 22, userName, password, alc);
+		this.setPort(port);
+	}
+
+	public int getPort() {
+		return this.port == 0 ? 22 : this.port;
+	}
+
+	public void setPort(String portString) {
+		try {
+			this.port = Integer.parseInt(portString);
+		} catch (NumberFormatException e) {
+			this.port = 22; // ±âº»°ª
+		}
 	}
 }
