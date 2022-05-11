2f6693abecb3869626fc27d45bf870b6fbb6fd81
KyeongYun Do <aservmz@gmail.com>
2022-05-12 00:31:35 +0900

    Setting springboot initial environment and create sample download web page

---
+++ DBMonitoringDownloaderApp/src/main/java/root/applications/Application.java
@@ -0,0 +1,35 @@
+package root.applications;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ComponentScan(basePackages = { "root.handler" })
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			log.info("Let's inspect the beans provided by Spring Boot");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				log.debug(beanName);
+			}
+		};
+	}
+}
