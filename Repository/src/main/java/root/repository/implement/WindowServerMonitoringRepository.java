cabf3e52edd7abf40edffa5a801952cf1e80dc3a
Dokyeongyun <aservmz@naver.com>
2022-05-25 00:02:57 +0900

    Monitoring window server OS disk usage

--- Repository/src/main/java/root/repository/implement/WindowServerMonitoringRepository.java
+++ Repository/src/main/java/root/repository/implement/WindowServerMonitoringRepository.java
@@ -1,5 +1,7 @@
 package root.repository.implement;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -50,7 +52,7 @@ public class WindowServerMonitoringRepository implements ServerMonitoringReposit
 		try {
 			String command = String.format("tail -%d %s", alc.getReadLine() - 1, alc.getReadFilePath());
 			result = jsch.executeCommand(command);
-			result = result.replace("\r\n", "\n");
+//			result = result.replace("\r\n", "\n");
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		}
@@ -60,6 +62,65 @@ public class WindowServerMonitoringRepository implements ServerMonitoringReposit
 
 	@Override
 	public List<OSDiskUsage> checkOSDiskUsage() {
-		return null;
+		List<OSDiskUsage> list = new ArrayList<>();
+		try {
+			String command = "wmic logicaldisk get deviceid,filesystem,size,freespace";
+			String result = jsch.executeCommand(command);
+			list = stringToOsDiskUsageList(result);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+
+		return list;
+	}
+
+	private List<OSDiskUsage> stringToOsDiskUsageList(String result) {
+		StringTokenizer st = new StringTokenizer(result);
+		List<String> header = Arrays.asList(new String[] { "DeviceID", "FileSystem", "FreeSpace", "Size" });
+		List<OSDiskUsage> list = new ArrayList<>();
+
+		boolean isHeader = true;
+		int index = 0;
+
+		OSDiskUsage row = new OSDiskUsage();
+		while (st.hasMoreElements()) {
+			String next = st.nextToken();
+			if (!isHeader) {
+				String headerName = header.get(index++);
+
+				switch (headerName) {
+				case "DeviceID":
+					row.setMountedOn(next);
+					break;
+				case "FileSystem":
+					row.setFileSystem(next);
+					break;
+				case "FreeSpace":
+					row.setFreeSpace(Double.valueOf(next));
+					break;
+				case "Size":
+					row.setTotalSpace(Double.valueOf(next));
+					break;
+				}
+
+				if (index == 4) {
+					double total = row.getTotalSpace();
+					double free = row.getFreeSpace();
+					double used = total - free;
+					row.setUsedSpace(used);
+					row.setUsedPercent(used / total * 100);
+
+					list.add(row);
+					row = new OSDiskUsage();
+					index = 0;
+				}
+			}
+
+			if (next.equals("Size")) {
+				isHeader = false;
+			}
+		}
+
+		return list;
 	}
 }
